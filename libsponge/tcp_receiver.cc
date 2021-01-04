10fc1ed85c4b23f10e72017e29a82a478ff62289
znhh6018 <44599853+znhh6018@users.noreply.github.com>
2021-01-04 23:27:56 +0800

    Update tcp_receiver.cc

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,27 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    //DUMMY_CODE(seg);
+    const TCPHeader& header = seg.header();
+    const Buffer& payLoad = seg.payload(); 
+    if(header.syn){
+        synSeqno = header.seqno;
+    }
+    if(!synSeqno.has_value()){
+        return;    
+    }
+    //no need to compare with the window size,has been designed in push_string
+    WrappingInt32 curSeqno = header.seqno;
+    uint64_t absolute_seq = unwrap(curSeqno,synSeqno,checkpoint);
+    string dataToPush = payLoad.copy();
+    _reassembler.push_string(dataToPush,absolute_seq,header.fin);
+    
+    
+    
+    
+    
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - unassembled_bytes() -  _reassembler.stream_out().buffer_size(); }
