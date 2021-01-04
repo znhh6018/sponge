2aea920fe828835956b84fb00a6a6da9268d856c
znhh6018 <44599853+znhh6018@users.noreply.github.com>
2021-01-05 07:33:43 +0800

    Update tcp_receiver.cc

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -14,15 +14,22 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     //DUMMY_CODE(seg);
     const TCPHeader& header = seg.header();
     const Buffer& payLoad = seg.payload(); 
-    if(header.syn){
+    //decide if connection start
+    if(header.syn && !synSeqno.has_value()){
         synSeqno = header.seqno;
+        ackSeqno = header.seqno;
     }
     if(!synSeqno.has_value()){
         return;    
     }
+    //connection start
+    size_t dataLength = payLoad.str().size() + header.syn?1:0 + header.fin?1:0;
+    
+    
+    
     //no need to compare with the window size,has been designed in push_string
     WrappingInt32 curSeqno = header.seqno;
-    uint64_t absolute_seq = unwrap(curSeqno,synSeqno,checkpoint);
+    uint64_t absolute_seq = unwrap(curSeqno,synSeqno,);
     string dataToPush = payLoad.copy();
     _reassembler.push_string(dataToPush,absolute_seq,header.fin);
     
