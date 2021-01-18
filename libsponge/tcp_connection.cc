598514a0cb93521a3ae8aa4a9a716eda55a6a32d
znhh6018 <44599853+znhh6018@users.noreply.github.com>
2021-01-18 23:39:32 +0800

    modify the push part and linger part,
    not sure about somthing.
    when should the activeFlag be true?
            send a syn 
            receive a syn
           send a syn with ack
    last time and  the last time ..

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -32,7 +32,19 @@ void TCPConnection::send_ACK_segment(WrappingInt32 ackno, uint16_t win) {
     ack_win_segment.header().win = win;
     _segments_out.push(ack_win_segment);
 }
-
+void TCPConnection::check_linger_after_send_and_recv(){
+	bool receive_finish = _receiver.stream_out().eof();
+        bool send_finish_and_acked = _sender.stream_in().eof() &&
+                                     _sender.next_seqno_absolute() == _sender.stream_in().bytes_written() + 2 &&
+                                     _sender.bytes_in_flight() == 0;
+        two_way_finish = receive_finish && send_finish_and_acked;
+        if (receive_finish && !send_finish_and_acked) {
+            _linger_after_streams_finish = false;
+        }
+	if (!_linger_after_streams_finish && send_finish_and_acked) {
+            activeFlag = false;
+        }
+}
 
 void TCPConnection::segment_received(const TCPSegment &seg) {
     // DUMMY_CODE(seg);
@@ -45,26 +57,18 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
     }
     //!RECV SEG
     _receiver.segment_received(seg);
+    if(seg.header().syn){
+	    activeFlag = true;
+    }
+    check_linger_after_send_and_recv();
     //!ACK
     if (seg.header().ack) {
         // tell sender win and ack
-        _sender.ack_received(seg.header().ackno, seg.header().win);
-        // check linger,receive finish while send not finish,no need to linger
-        bool receive_finish = _receiver.stream_out().eof();
-        bool send_finish_and_acked = _sender.stream_in().eof() &&
-                                     _sender.next_seqno_absolute() == _sender.stream_in().bytes_written() + 2 &&
-                                     _sender.bytes_in_flight() == 0;
-        two_way_finish = receive_finish && send_finish_and_acked;
-        if (receive_finish && !send_finish_and_acked) {
-            _linger_after_streams_finish = false;
-        }
+        _sender.ack_received(seg.header().ackno, seg.header().win);       
         // check active
         if (_sender.next_seqno_absolute() > _sender.bytes_in_flight()) {
             activeFlag = true;
-        }
-        if (!_linger_after_streams_finish && send_finish_and_acked) {
-            activeFlag = false;
-        }
+        }  
         if (_linger_after_streams_finish && two_way_finish) {
             if (!timer_for_linger.has_value()) {
                 timer_for_linger = 0;
@@ -90,6 +94,7 @@ size_t TCPConnection::write(const string &data) {
     size_t bytes_write = _sender.stream_in().write(data);
     _sender.fill_window();
     push_from_Sender_to_connection();
+    check_linger_after_send_and_recv();
     return bytes_write;
 }
 
@@ -127,6 +132,7 @@ void TCPConnection::connect() {
     if (_sender.next_seqno_absolute() == 0) {
         _sender.fill_window();
         push_from_Sender_to_connection();
+	activeFlag = true;
     }
 }
 
@@ -134,26 +140,33 @@ TCPConnection::~TCPConnection() {
     try {
         if (active()) {
             cerr << "Warning: Unclean shutdown of TCPConnection\n";
-
             // Your code here: need to send a RST segment to the peer
             TCPSegment RST_segment;
             RST_segment.header().seqno = _sender.next_seqno();
             RST_segment.header().rst = true;
             _segments_out.push(RST_segment);
-            // _sender.stream_in().set_error();
-            // _receiver.stream_out().set_error();
-            // activeFlag = false;
         }
     } catch (const exception &e) {
         std::cerr << "Exception destructing TCP FSM: " << e.what() << std::endl;
     }
 }
 
-
 void TCPConnection::push_from_Sender_to_connection() { 
-	queue<TCPSegment> &sender_queue = _sender.segments_out();
+    queue<TCPSegment> &sender_queue = _sender.segments_out();
     while (!sender_queue.empty()) {
-        _segments_out.push(sender_queue.front());
-        sender_queue.pop();
+	TCPSegment& sender_segment = sender_queue.front() ;
+	if(_receiver.ackno().has_value()){
+		uint16_t win{0};
+        	if (_receiver.window_size() > numeric_limits<uint16_t>::max()) {
+            		win = numeric_limits<uint16_t>::max();
+        	} else {
+            		win = static_cast<uint16_t>(_receiver.window_size());
+        	}
+		sender_segment.header().ack = true;
+    		sender_segment.header().ackno = ackno;
+   		sender_segment.header().win = win;
 	}
+        _segments_out.push(sender_segment);
+        sender_queue.pop();
+    }
 }
